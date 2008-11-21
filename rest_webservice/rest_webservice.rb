e8fa2e0fe9e01213d0a0e0b162ef7c6467e95f6e
Jason Goecke <jsgoecke@mayluc.local>
2008-11-21 08:22:31 -0800

    Added the ability to have an exception list of NPANXX for destination numbers when a long distance code should or should not be added to the destination number

--- rest_webservice/rest_webservice.rb
+++ rest_webservice/rest_webservice.rb
@@ -15,23 +15,37 @@ adhearsion = DRbObject.new_with_uri "druby://#{$config["ahn_drb_hostname"]}:#{$c
 
 #Format the number in order to ensure it is for SIP or IAX2 or even TDM
 def format_source source
-  return $config["channel_technology"] + '/' + source
+  return $config["source_technology"] + '/' + source.to_s
+end
+
+#Verify the final dial string prepending the prefix and long distance code
+def format_destination phone_number
+  #See if the number is on the exception list and then either add only a dial prefix if it is
+  #or a long distance prefix if it is not
+  if phone_number.to_s.match($config["exception_list"])
+    phone_number = $config["dial_prefix"].to_s + phone_number.to_s
+  else
+    phone_number = $config["dial_prefix"].to_s + $config["long_distance_prefix"].to_s + phone_number.to_s
+  end
+  
+  #Add the outbound trunk if it is present in the configuration
+  if $config["dial_trunk"] != nil
+    phone_number = phone_number.to_s + "@" + $config["dial_trunk"].to_s
+  end
+
+  return phone_number
 end
 
 #Expose the RESTFful API via Sinatra web server
 post "/call" do  
   
-  #Add the outbound trunk if it is present in the configuration
-  if $config["dial_trunk"] != nil
-    channel = format_source(params[:source]) + "@" + $config["dial_trunk"]
-  else
-    channel = format_source(params[:source])
-  end
+  source = format_source(params[:source])
+  destination = format_destination(params[:destination])
   
   #Build the options to place the call
-  options = { :channel => channel,
+  options = { :channel => source,
               :context => $config["click_to_call_context"],
-              :exten => params[:destination],
+              :exten => destination,
               :priority =>  1,
               :timeout => 43200000,
               :async => $config["dial_async"] }
